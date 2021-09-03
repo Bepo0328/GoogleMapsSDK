4c94075e4cfdc405dd1d155130f99dede99f7118
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-09-03 18:49:16 +0900

    GoogleMapsSDK: MapType 및 MapStyle 변경
    # Normal, Hybrid, Satellite, Terrain, None Type 추가
    # Json을 이용해 MapStyle 변경
    # 한국은 Style 변경이 불가

--- app/src/main/java/kr/co/bepo/googlemapssdk/MapsActivity.kt
+++ app/src/main/java/kr/co/bepo/googlemapssdk/MapsActivity.kt
@@ -2,12 +2,16 @@ package kr.co.bepo.googlemapssdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import kr.co.bepo.googlemapssdk.databinding.ActivityMapsBinding
 
@@ -28,15 +32,34 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_types_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.normal_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+            R.id.hybrid_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+            }
+            R.id.satellite_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            }
+            R.id.terrain_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            }
+            R.id.none_map -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_NONE
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -45,6 +68,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10f))
         mMap.uiSettings.apply {
             isZoomControlsEnabled = true
+        }
+        setMapStyle(mMap)
+    }
+
+    private fun setMapStyle(googleMap: GoogleMap) {
+        try {
+            val success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this,
+                    R.raw.style
+                )
+            )
+
+            if (!success) {
+                Log.d("Maps", "Failed to add Style.")
+            }
+        } catch (e: Exception) {
+            Log.d("Maps", e.toString())
         }
     }
 }
