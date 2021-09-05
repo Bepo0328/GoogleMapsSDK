896dce8571cedc86386bf320734bcb6615fa5870
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-09-05 19:34:44 +0900

    GoogleMapsSDK: Polyline 추가

--- app/src/main/java/kr/co/bepo/googlemapssdk/MapsActivity.kt
+++ app/src/main/java/kr/co/bepo/googlemapssdk/MapsActivity.kt
@@ -1,25 +1,37 @@
 package kr.co.bepo.googlemapssdk
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kr.co.bepo.googlemapssdk.databinding.ActivityMapsBinding
 import kr.co.bepo.googlemapssdk.databinding.CustomInfoWindowBinding
 import kr.co.bepo.googlemapssdk.misc.CameraAndViewport
 import kr.co.bepo.googlemapssdk.misc.CustomInfoAdapter
 import kr.co.bepo.googlemapssdk.misc.TypeAndStyle
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPolylineClickListener {
 
     private lateinit var mMap: GoogleMap
     private val binding: ActivityMapsBinding by lazy { ActivityMapsBinding.inflate(layoutInflater) }
+
+    private val losAngeles = LatLng(34.04692127928215, -118.24748421830992)
+    private val newYork = LatLng(40.71614203933524, -74.00440676650565)
+    private val madrid = LatLng(40.639871895206674, -3.5627974558481665)
+    private val panama = LatLng(8.457442357239337, -79.93696458060398)
 
     private val typeAndStyle: TypeAndStyle by lazy { TypeAndStyle() }
     private val cameraAndViewport: CameraAndViewport by lazy { CameraAndViewport() }
@@ -46,8 +58,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val losAngeles = LatLng(34.04692127928215, -118.24748421830992)
-        val newYork = LatLng(40.71614203933524, -74.00440676650565)
         val losAngelesMarker =
             mMap.addMarker(
                 MarkerOptions()
@@ -61,8 +71,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             isZoomControlsEnabled = true
         }
 
-        mMap.setInfoWindowAdapter(CustomInfoAdapter(this))
-
         typeAndStyle.setMapStyle(mMap, this)
+
+        mMap.setOnPolylineClickListener(this)
+
+        lifecycleScope.launch {
+            addPolyline()
+        }
+    }
+
+    private suspend fun addPolyline() {
+        val polyline = mMap.addPolyline(
+            PolylineOptions().apply {
+                add(losAngeles, newYork, madrid)
+                width(5f)
+                color(Color.BLUE)
+                geodesic(true)
+                clickable(true)
+            }
+        )
+
+        delay(5_000)
+
+        val newList = listOf<LatLng>(
+            losAngeles, panama, madrid
+        )
+
+        polyline.points = newList
+    }
+
+    override fun onPolylineClick(polyline: Polyline) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
     }
 }
